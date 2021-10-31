b632b5079c686caf4346b18ad307faa42bf5265a
Gene He <genehe3@gmail.com>
2021-10-31 07:43:31 -0700

    removed stylesheet. set up canvas variables

--- snake.js
+++ snake.js
@@ -1,1 +1,7 @@
-console.log('Hello World!')
+let winInnerWidth = window.innerWidth; //setting entire window width as a variable
+let winInnerHeight = window.innerHeight; //setting entire window height as a variable
+let theBoard = document.querySelector('canvas'); //setting the game board as a variable
+let ctx = theBoard.getContext('2d'); //context is the "pen" that draws on the canvas
+
+
+
