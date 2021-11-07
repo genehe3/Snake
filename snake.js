926eb0a114489ee1f8e6c6afde879c157414a191
Gene He <genehe3@gmail.com>
2021-11-07 10:41:26 -0800

    arrowkey and edge detection, move to other side

--- snake.js
+++ snake.js
@@ -26,23 +26,71 @@ theBoard.width = window.innerWidth;
 
 let x = theBoard.width/2 //sets starting point center
 let y = theBoard.height/2 //sets starting point to center
+let dx = 6 //the horizontal "velocity"; 5 pixels per 
+let dy = 0 //the vertical "velocity"; starts at 0
 //need to animate a rect going right and bouncing off
 const animate = () => {
    ctx.strokeStyle = '#ff0000'; 
    ctx.lineWidth = 7;
-   ctx.clearRect(0, 0, innerWidth, innerHeight); // clears before drawing, hitting the whole screen
+   ctx.clearRect(0, 0, theBoard.width, theBoard.height); // clears before drawing, hitting the whole screen
    ctx.beginPath();//initiate the first square
    ctx.rect(x, y, 30, 30);//centers square
    ctx.stroke();
-   x += 5; // increase the x value by 1, moving it right one pixel
-   if (x + 15 === innerWidth) {
+   x += dx; // increase the x value by 5, moving it right one pixel
+   y += dy; // increase y value 
+   if (x + 12 >= theBoard.width && dx === 6) {
+       //detects when rectangle hits the RIGHT edge, x-position(center of square) plus half the width (15) 
        x = 0;
    }; //when the square hits the edge, it jumps to the other side
    //ctx.clearRect(), doesn't work as it clears right after drawing, effectively doing nothing
+
+   if (y + 12 <= 0 && dy === -6) {
+       //detects when rectangle hits the TOP 
+       y = theBoard.height;
+   };
+
+   
+   if (y + 12 >= theBoard.height && dy === 6) {
+    //detects when rectangle hits the BOTTOM
+    y = 0;
+};
+
+
+if (x + 12 <= 0 && dx === -6) {
+    //detects when rectangle hits the LEFT
+    x = theBoard.width;
+};
+
+   
    requestAnimationFrame(animate); //calls animate function again, works but need to clear previous rectangles before redrawing (done)
-}
+};
 
 animate();
+
+//add arrow key press detection
+document.addEventListener("keydown", (direct) => {
+    //"listens" for a key press and passes the key as a parameter into the following function
+    //console.log(direct.key) // detects the code for key which was pressed; returns "ArrowRight", "ArrowLeft", "ArrowUp", and "ArrowDown"
+    if (direct.key === "ArrowUp") {
+        dx = 0;
+        dy = -6;
+    };
+
+    if (direct.key === "ArrowDown") {
+        dx = 0;
+        dy = 6;
+    }
+
+    if (direct.key === "ArrowLeft") {
+        dx = -6;
+        dy = 0;
+    }
+
+    if (direct.key === "ArrowRight") {
+        dx = 6;
+        dy = 0;
+    }
+})
 
 
 
